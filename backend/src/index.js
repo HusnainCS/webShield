263b3eb20c8ec0f593d3ed48953076fa8aa968df
Husnain <husnian.cs@gmail.com>
2025-12-10 20:30:37 +0500

    integrated frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,13 @@ import userRouter from "./routers/users-router.js";
 import cookieParser from "cookie-parser";
 import scanRouter from "./routers/scans-router.js";
 import authRouter from "./routers/auth-router.js";
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
