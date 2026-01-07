59fad2e3310225a9baf6454780602fa8d102c746
husnain <husnain@husnain.husnain>
2026-01-07 12:01:50 +0500

    Backend ready for deployment, uses environment variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
